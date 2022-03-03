17f31a5d724c73618383228ce38f5b07f4b7a5ef
Marianne <marianneneuts@gmail.com>
2022-03-03 14:46:35 +0100

    feat: add reset() function

--- lab3/classes/App.js
+++ lab3/classes/App.js
@@ -3,33 +3,31 @@ import Todo from "./Todo";
 export default class App {
     constructor() {
       console.log("🍕");
+
       // HINT🤩
       // set up the enter Key
-      this.setupEventListeners();
+      // this.setupEventListeners();
       // when the app loads, we can show previously saved items from localstorage
       // this.loadFromStorage();
+
+      this.setupEventListeners();
+      this.loadFromStorage();
     }
   
     setupEventListeners() {
       console.log("👂🏽");
+
       // HINT🤩
-      document.querySelector("#add-item-text").addEventListener("keyup", this.createItem.bind(this));
       // pressing the enter key in the text field triggers the createItem function
       // addEventListener("keyup", this.createItem.bind(this));
       // read up on .bind() -> we need to pass the current meaning of this to the eventListener
       // while testing, feel free to console.log(this) to see what's in it
+
+      document.querySelector("#add-item-text").addEventListener("keyup", this.createItem.bind(this));
+      // console.log(this);
     }
   
     createItem(e) {
-      if(e.key === "Enter") {
-        console.log("📕");
-        let todo = new Todo("This is a demo!");
-        todo.add();
-      }
-
-      // this.reset();
-      console.log(this);
-
       // HINT🤩
       // this function should create a new todo by using the Todo() class
       // new Todo(text)
@@ -38,6 +36,16 @@ export default class App {
       // if you used bind() in the previous function, you'll notice that this refers to the current class instance
       // clear the text field with .reset() after adding the item
       // if (e.key === "Enter")
+
+      if(e.key === "Enter") {
+        
+        console.log("📕");
+        let todo = new Todo();
+        todo.add();
+        // console.log(todo);
+
+        this.reset();
+      }
     }
   
     loadFromStorage() {
@@ -48,6 +56,8 @@ export default class App {
   
     reset() {
       // this function should reset the form / clear the text field
+
+      document.querySelector("#add-item-text").value = "";
     }
   }
   
